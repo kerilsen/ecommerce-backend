17c25949a9f85a73b0e0e0d839e731d1f411197d
Keri Sen <147566619+kerilsen@users.noreply.github.com>
2024-03-04 17:06:41 -0800

    Had to make a change to use env

--- config/connection.js
+++ config/connection.js
@@ -3,10 +3,9 @@ require('dotenv').config();
 const Sequelize = require('sequelize');
 
 const sequelize = 
-// process.env.JAWSDB_URL
-//   ? new Sequelize(process.env.JAWSDB_URL)
-//   :
-   new Sequelize('ecommerce_db', 'root', 'Janine1982*', {
+process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
       host: 'localhost',
       dialect: 'mysql',
       dialectOptions: {
